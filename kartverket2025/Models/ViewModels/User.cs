bd3c646ef8f3d2e5a71d80bea29de50eeff7a54e
jaeyua <jaeyua@users.noreply.github.com>
2025-06-22 15:46:48 +0100

    changing the username to fullname

--- kartverket2025/Models/ViewModels/User.cs
+++ kartverket2025/Models/ViewModels/User.cs
@@ -3,7 +3,7 @@
     public class User
     {
         public string Id { get; set; }
-        public string UserName { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
         public IList<string> Roles { get; set; }
     }

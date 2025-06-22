08b12970f7f7641d76cecbd5f4725943d3e49e1c
jaeyua <jaeyua@users.noreply.github.com>
2025-06-22 15:45:40 +0100

    latest added migrations (tile layers)

--- kartverket2025/Migrations/20250622143751_AddTileLayerOrKartTypeToDb.Designer.cs
+++ kartverket2025/Migrations/20250622143751_AddTileLayerOrKartTypeToDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using kartverket2025.Data;
 
@@ -11,9 +12,11 @@ using kartverket2025.Data;
 namespace kartverket2025.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250622143751_AddTileLayerOrKartTypeToDb")]
+    partial class AddTileLayerOrKartTypeToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

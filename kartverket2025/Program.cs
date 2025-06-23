4f23dff1ab2c3962129e5726180f2518820276ea
jaeyua <jaeyua@users.noreply.github.com>
2025-06-23 14:30:38 +0100

    added configurations on the identity applicationuser model and cookies and CSP security

--- kartverket2025/Program.cs
+++ kartverket2025/Program.cs
@@ -1,8 +1,10 @@
 using kartverket2025.Data;
 using kartverket2025.Models.DomainModels;
+using kartverket2025.Models.ViewModels;
 using kartverket2025.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,11 +18,39 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     )
 );
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
 builder.Services.AddScoped<IMapReportRepository, MapReportRepository>();
 builder.Services.AddScoped<IUsersListRepository, UsersListRepository>();
+
+
+// Configure identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireUppercase = false;
+
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(25);
+    options.Lockout.MaxFailedAccessAttempts = 10;
+    options.Lockout.AllowedForNewUsers = true;
+
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+// Configure authentication and authorization
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login/";
+    options.AccessDeniedPath = "/Shared/AccessDenied/";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+    options.SlidingExpiration = true;
+    options.Cookie.HttpOnly = true;
+});
 
 var app = builder.Build();
 
@@ -30,12 +60,12 @@ using (var scope = app.Services.CreateScope())
     try
     {
         var context = services.GetRequiredService<ApplicationDbContext>();
-        context.Database.Migrate();  // Only for use in dev with Docker Compose
+        context.Database.Migrate(); 
     }
     catch (Exception ex)
     {
-        Console.WriteLine("Migration failed.");
-        Environment.Exit(1); // Graceful shutdown
+        Console.WriteLine("Seeding or migration fail");
+        Environment.Exit(1); 
     }
 }
 
@@ -46,6 +76,26 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+// enable CSP security 
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
+    context.Response.Headers.Append("X-Frame-Options", "DENY");
+    context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+    context.Response.Headers.Append("Referrer-Policy", "strict-origin-when-cross-origin");
+    context.Response.Headers.Append("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+
+    context.Response.Headers.Append("Content-Security-Policy",
+        "default-src 'self'; " +
+        "script-src 'self' https://cdnjs.cloudflare.com/ https://unpkg.com/ https://cdn.jsdelivr.net/ 'unsafe-inline' 'unsafe-eval'; " +
+        "style-src 'self' https://cdnjs.cloudflare.com/ https://unpkg.com/ https://fonts.googleapis.com/ 'unsafe-inline'; " +
+        "font-src 'self' https://fonts.gstatic.com/ https://ka-f.fontawesome.com/ https://kit.fontawesome.com/ data:; " +
+        "img-src 'self' data: https:; " +
+        "connect-src 'self' wss://localhost:* https://api.kartverket.no/ https://%2A.kartverket.no/ https://ka-f.fontawesome.com/ https://nominatim.openstreetmap.org/; " +
+        "object-src 'none';");
+
+    await next();
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();

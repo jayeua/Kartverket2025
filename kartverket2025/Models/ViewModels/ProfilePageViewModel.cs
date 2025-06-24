792bd5370c9f8537ef345d6fc035af1860382ae4
jaeyua <jaeyua@users.noreply.github.com>
2025-06-24 17:00:13 +0100

    added new models for changing password and forget pass

---
+++ kartverket2025/Models/ViewModels/ProfilePageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace kartverket2025.Models.ViewModels
+{
+    public class ProfilePageViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ kartverket2025/Models/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kartverket2025.Models.ViewModels
+{
+    public class SettingsViewModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
